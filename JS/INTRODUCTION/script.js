509605a6b03cb4d9d53cb2d096bae59aca15ef9e
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-03 23:40:36 +0500

    completed

---
+++ JS/INTRODUCTION/script.js
@@ -0,0 +1,28 @@
+// programming language used in web dev 
+// controlling Behaviour
+// case sensitive language (name or Name are different) 
+
+
+// console for developers used for debugging
+// alert for users and used for input or alert 
+
+// data types
+// String (for text "My name is jawad")
+// Number 
+// Float (decimal numbers)
+// Boolean (true false)
+// ; is called terminator
+
+// console
+
+console.log("My name is Jawad");
+console.log(2 + 5);
+console.log(2 * 5);
+console.log(5 - 4);
+
+// alert
+
+alert("Hello, Jawad!");
+alert(2 + 2);
+
+
