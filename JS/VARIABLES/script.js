fc257bba1f9910f4bfb6168811d9eb92f1de0d2f
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-05 23:08:36 +0500

    updated

---
+++ JS/VARIABLES/script.js
@@ -0,0 +1,29 @@
+// Variables as container for storing value
+// declare krty hyn
+// var , let , const
+
+var num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+//typeof function is built-in function of JS used to check data type of a variable
+console.log(typeof(num1));
+
+
+
+var Fname = "Jawad";
+var Lname = "Safdar";
+// concatenate
+console.log(Fname + " " + Lname);
+
+var num3 = "10";
+var num4 = "20";
+console.log(num3 + num4);
+console.log(typeof(num3));
+
+
+var isLoggedin = true;
+console.log(typeof(isLoggedin));
+
+var isbanned = false;
+console.log(typeof(isbanned));
+
