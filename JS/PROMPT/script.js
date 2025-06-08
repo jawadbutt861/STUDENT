41160bf5386394c4a41f0a6e339d62ff64ba6be6
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-08 23:47:41 +0500

    updated

---
+++ JS/PROMPT/script.js
@@ -0,0 +1,38 @@
+// prompt user sy input lyny k liay
+
+// var marks = prompt("Enter Your Marks");
+// alert("Your Marks are " + marks);
+
+// var num1 = prompt("Enter value of Number 1");
+// var num2 = prompt("Enter value of Number 2");
+//alert(num1 + num2);
+//console.log(typeof(num1));
+// built-in function parseInt(); string ko number me convert krta h
+
+// alert(parseInt(num1) + parseInt(num2));
+
+var num1 = "45";
+console.log(typeof(num1));
+console.log(parseInt(num1,16));
+console.log(typeof(num1));
+
+
+
+
+// var name = prompt("Enter Your Name");
+// var fname = prompt("Enter Father Name");
+// var uni = prompt("Enter University Name");
+// var semester = prompt("Enter Your Semester");
+
+// alert("It is Certified that " + name + " son of " + fname + " is student of " + uni + " University and currently enrolled in " + semester + " Semester");
+
+// Re-Declare
+var marks = 30; // Variable Declaration
+
+var marks = 40; // Re-Declaration
+alert(marks);
+
+// Re-Assign
+var fname = "Jawad";
+fname = "Ali";
+alert(fname);
