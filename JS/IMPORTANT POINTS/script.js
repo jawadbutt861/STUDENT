a6fa8c5856b2364e39e728d2d3d86425f7f6337b
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-10 13:17:53 +0500

    updated

---
+++ JS/IMPORTANT POINTS/script.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+console.log("Hello World!");
+
+let firstName = "Jawad";
+let lastName = "Safdar";
+
+// let firstName = "Laiba";
+firstName = "Laiba";
+console.log(firstName);
+
+const num = 20;
+// const num = 20;
+// num = 30;
+console.log(num);
+
+let num2;
+console.log(num2);
+
+// const num4 ;
+
---
+++ JS/OBJECT/script.js
@@ -0,0 +1,44 @@
+let student1 = 
+{
+//  key : value , 
+    name : "Jawad",
+    ID : 62024,
+    CNIC : 3440264272395,
+    Semester : "6th"
+};
+
+let student2 = 
+{
+//  key : value , 
+    name : "Ali",
+    ID : 60024,
+    CNIC : 3440264272395,
+    Semester : "6th"
+};
+
+let student3 = 
+{
+//  key : value , 
+    name : "Laiba",
+    ID : 60023,
+    CNIC : 3440264272395,
+    Semester : "6th"
+};
+
+let student4 = 
+{
+//  key : value , 
+    name : "Samraan",
+    ID : 60060,
+    CNIC : 3440264272395,
+    Semester : "6th"
+};
+
+// console.log(student);
+console.log(student3);
+console.log(student4.ID);
+console.log(student1["name"]);
+
+
+
+
