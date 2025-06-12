7a5b569a3b9bf916e50505a417a61dc840cd677a
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-12 23:16:00 +0500

    updated

---
+++ JS/OPERATORS/script.js
@@ -0,0 +1,52 @@
+// Ariithemetic Operators (+ - * / %)
+// Assignmet Operators (= += -= *= /=)
+// Comparison Operators (< > <= >= == === !== !=)
+// Logical Operators (&& || !)
+// Bitwise operators
+// Unary operator
+// ternary operator
+
+// Ariithemetic Operators (+ - * / %)
+
+// let num1 = 5;
+// let num2 = 5;
+// console.log("Sum is ", num1 + num2);
+// console.log("Subtraction is ", num1 - num2);
+// console.log("Division is ", num1 / num2);
+// console.log("Multiplication is ", num1 * num2);
+// console.log("Reminder is ", num1 % num2);
+
+
+// Assingnmet Operators (= += -= *= /=)
+
+// let num3 = 20;
+
+// console.log(num3 += 5); // num3 = num3 + 5 (num3 = 20 + 5)
+// console.log(num3 -= 5); // num3 = num3 - 5 (num3 = 20 - 5)
+// console.log(num3 *= 5); // num3 = num3 - 5 (num3 = 20 * 5)
+// console.log(num3 /= 5); // num3 = num3 - 5 (num3 = 20 / 5)
+
+
+// Comparison Operators (< > <= >= == === !== !=)(always returns a boolean value)
+
+// let num1 = 9;
+// let num2 = "10";
+
+// console.log(num1 < num2);// (5 < 10)
+// console.log(num1 > num2);// (5 > 10)
+// console.log(num1 >= num2);// (10 >= 10) (bra ho ya braber ho)
+// console.log(num1 <= num2);// (11 <= 10) (choota ho ya braber ho)
+
+// console.log(num1 == num2);// (11 == 10) (sirf data/value ko compare krta h)
+// console.log(num1 === num2); // (data/value or data type dono ko compare krta h)
+
+// console.log(num1 != num2);// (10 != 10)(sirf data/value ko compare krta h)
+
+// console.log(num1 !== num2);// (9 !== "10")(data/value or data type dono ko compare krta h)
+
+
+
+
+
+
+
