1088f213f37607ca0896239b8373883480758904
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-14 23:40:24 +0500

    updated

--- JS/OPERATORS/script.js
+++ JS/OPERATORS/script.js
@@ -45,7 +45,64 @@
 // console.log(num1 !== num2);// (9 !== "10")(data/value or data type dono ko compare krta h)
 
 
+// Logical operators
+// && (AND Operator )
+// 1 b condition false hui to sb conditions false ho jyn gi
 
+console.log(5 === 5 && 'a' === 'a');
+            // true && true
+            //      true
+
+console.log(3 === 3 && 4 !== 4);
+            // true && false
+            // false
+            // Agr 1 b false hua to sary false ho jyn gy
+console.log(2 + 2 === 4 && 3 * 3 === 9);
+            // true && true
+            // true
+console.log(10 - 3 > 5 && 2 * 2 === 4);
+            // true &&  true
+            // true
+console.log(typeof 'JS' === 'string');
+            // string === string
+            // true
+        //  TRUTH TABLE
+            // T  T = T
+            // T  F = F
+            // F  T = F
+            // F  F = F
+
+// OR operator (||)
+// dono me sy 1 true ho gyi to sb true ho jyn gi
+// boolean true/false  0/1(binary numbers)  1 represents true and 0 represents false
+// TRUTH TABLE
+// T T = T
+// T F = T
+// F T = T
+// F F = F
+
+console.log('red' !== 'blue' || 7 < 5);
+        //  true || false
+        //   true
+
+console.log(5 + 3 > 7 || 2 * 2 !== 4);
+        //  true || false
+        //      true
+
+console.log(6 / 2 === 3 && 'cat' === 'dog' || 1);
+        //  true && false || 1
+        //  false || 1(true)
+        // true
+
+// Not Operator !
+// agr true h to false return kry ga or agr false h to true return kry ga
+// TRUTH TABLE
+// T = F
+// F = T
+
+console.log(!(5 > 10));
+        //  !false
+        // true
 
 
 
