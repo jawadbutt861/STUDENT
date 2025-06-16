bf6ed55329371cd859aa0417246720f1dccc57ba
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-16 23:30:25 +0500

    updated

---
+++ JS/CONDITIONAL STATEMENTS/script.js
@@ -0,0 +1,95 @@
+// let age = prompt("Enter your Age");
+// if-else
+// if(age >= 18)
+// { // block of statements
+//     console.log("Eligible");
+//     console.log("Welcome");
+// }
+// else
+// {
+//     console.log("Not Eligile");
+//     console.log("You are not Welcomed");
+// }
+
+// let num = prompt("Enter a number");
+// if(num %2 === 0)
+// {
+//     console.log("Even");
+// }
+// else
+// {
+//     console.log("Odd"); 
+// }
+
+// let year = prompt("Enter Year");
+
+// if(year %4 === 0)
+// {
+//     console.log("Leap Year");
+// }
+// else
+// {
+//     console.log("Not a Leap Year");
+// }
+
+// user sy 1 number lyna h or pta krna h k us ny +ve enter kiya h ya -ve
+
+// var num =  prompt("Enter a Number");
+
+// if(num > 0)
+// {
+//     console.log("Positive");
+// }
+// else
+// {
+//     console.log("Negative");
+// }
+
+//Else-if
+
+// if(num > 0)
+// {
+//     console.log("Positive");
+// }
+// else if(num < 0)
+// {
+//     console.log("Negative");
+// }
+// else
+// {
+//     console.log("You Entered Zero");
+// }
+
+
+// let marks =  prompt("Enter Marks");
+
+// if(marks >= 90 && marks <= 100)
+// {
+//     console.log("A+");
+// }
+// else if(marks >= 80 && marks < 89)
+// {
+//     console.log("A");
+// }
+// else if(marks >= 70 && marks < 79)
+// {
+//     console.log("B");
+// }
+// else if(marks >= 60 && marks < 69)
+// {
+//     console.log("C");
+// }
+// else if(marks >= 50 && marks < 59)
+// {
+//     console.log("D");
+// }
+// else if(marks >= 0 && marks < 50)
+// {
+//     console.log("F");
+// }
+// else
+// {
+//     console.log("Invalid marks");
+// }
+
+
