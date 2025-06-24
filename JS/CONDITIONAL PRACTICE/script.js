94fd3d0bddf04a8d024720b00f06c57320d87e15
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-24 23:44:51 +0500

    updated

---
+++ JS/CONDITIONAL PRACTICE/script.js
@@ -0,0 +1,65 @@
+// let age = 10;
+// let country = "India";
+// let isBanned = false;
+
+// if((age >= 18 && age <= 35) && (country == "Pakistan" || country == "India") && (isBanned == false))
+// {
+//     console.log("Eligible");
+// }
+// else
+// {
+//     console.log("Not Eligible");
+    
+// }
+
+
+// let plan = "premium";
+// let login = true;
+// if(plan == "premium" && login == true)
+// {
+//     console.log("Welcome premium");
+// }
+// else if(plan == "free" && login == true)
+// {
+//     console.log("Welcome Free User");
+// }
+// else if(login == false)
+// {
+//     console.log("Please Login");
+// }
+// else
+// {
+//     console.log("Invalid Subscription");
+// }
+
+
+
+// let price = 500;
+// let inStock = true;
+// let isMember = false;
+// if(price > 1000 && inStock == true && isMember == true)
+// {
+//     console.log("Buy now with discount");
+// }
+// else if(inStock == true)
+// {
+//     console.log("Buy Now");
+// }
+// else if(inStock == false)
+// {
+//     console.log("Out of Stock");
+// }
+// else{
+//     console.log("Invalid Product");
+// }
+
+// let hoursWorked = 20;
+// let isWeekend = true;
+
+// if(hoursWorked > 40 && isWeekend == false)
+// {
+
+// }
+// else if (hoursWorked > 0 && isWeekend == false)
+
+
