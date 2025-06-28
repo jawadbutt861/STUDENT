230a423dface692c9eafe92cb4e24baac9433dec
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-29 00:04:27 +0500

    updated

---
+++ JS/FUNCTION/script.js
@@ -0,0 +1,26 @@
+// block of code
+// let num1 = 20;
+// let num2 = 40;
+// console.log(num1 + num2);
+
+function sum() //Function Declaration
+{
+    let num1 = 20;
+    let num2 = 40;
+    console.log(num1 + num2);
+}
+
+sum(); // Function call
+sum();
+sum();
+sum();
+sum();
+
+let name = "Jawad";
+let age = 22;
+
+console.log("Hello my name is " + name + " and my age is " + age );
+
+// String Literals
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
