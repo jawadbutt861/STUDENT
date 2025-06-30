b86fc41358096d9a5c723f2b965909447d5734d0
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-30 23:39:26 +0500

    updated

--- JS/FUNCTION/script.js
+++ JS/FUNCTION/script.js
@@ -1,26 +1,182 @@
-// block of code
+// block of code piece of code 
+// 1 hi code br br na likhna pry
+// code ko  reuse krny k liay
 // let num1 = 20;
 // let num2 = 40;
 // console.log(num1 + num2);
 
-function sum() //Function Declaration
-{
-    let num1 = 20;
-    let num2 = 40;
-    console.log(num1 + num2);
-}
+// function sum() //Function Declaration
+// {
+//     let num1 = 20;
+//     let num2 = 40;
+//     console.log(num1 + num2);
+// }
 
-sum(); // Function call
-sum();
-sum();
-sum();
-sum();
+// sum(); // Function call
+// sum();
+// sum();
+// sum();
+// sum();
 
-let name = "Jawad";
-let age = 22;
+// let name = "Jawad";
+// let age = 22;
 
-console.log("Hello my name is " + name + " and my age is " + age );
+// console.log("Hello my name is " + name + " and my age is " + age );
 
 // String Literals
 
-console.log(`Hello my name is ${name} and my age is ${age}`);
+// console.log(`Hello my name is ${name} and my age is ${age}`);
+
+
+// Parameterized function
+
+// function sum(num1,num2){ //Parameters // parameter pass krty hyn (variables jo hm function me pass krty hyn or jo value get krty)
+//     console.log(num1 + num2);
+// }
+
+// sum(8,6); // Arguments
+
+
+// function greetUser(name,age,email){
+//     // console.log(`Hello ${name}. Your Age is ${age} . Your Email is ${email}`);
+//     // console.log("Hello World");
+    
+//     return `Hello ${name}. Your Age is ${age} . Your Email is ${email}`; // function ka result return krta h
+// }
+
+// console.log(greetUser("Laiba",25,"laibibabybutt134@gmal.com"));
+
+
+// let name = prompt("Enter Your Name");
+// let age = prompt("Enter Your age");
+// let email = prompt("Enter Your Email");
+
+// console.log(greetUser(name,age,email));
+
+
+// return
+
+
+// Variables Datatypes primitive and non-primitive object operators conditions function
+// array loops dom manipulation 
+
+function evenOdd(num){
+    if(num %2 == 0){
+        return "Even";
+    }
+    else{
+        return "Odd";
+    }
+}
+
+console.log(evenOdd(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
