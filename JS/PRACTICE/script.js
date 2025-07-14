25a2c44d9be3c405b626c464d19eda8a132081ae
Jawadbutt861 <jawadbutt861@gmail.com>
2025-07-14 23:17:10 +0500

    updated

---
+++ JS/PRACTICE/script.js
@@ -0,0 +1,56 @@
+// variables
+// Datatypes
+// operators
+// conditions
+
+// let const var
+
+// let (redeclare ni kr skty reassign kr skty hyn)
+// const (redeclare ni kr skty na hi reassign kr skty hyn)
+// var (reassign kr skty hyn redeclare b kr skty hyn)
+// log debugging k liay use hota h programmers or developers krty hyn
+// alert alert krny k liay or pop up show hota h
+
+// Number boolean float String (primitive)
+// Object,Arrays (Non-primitive)
+
+// data store krny k liay
+
+// + - * / % (Arithmetic)
+// || && ! (Logical)
+// += -= (Assignment)
+// >= <= > < (Comparison)
+
+// ===  ==
+
+// "a" === "A" && "red" == "Red"
+// false && true
+
+// object (collection of data)
+
+// let Car = {
+//     model : 2020,
+//     color : "red"
+// };
+
+// console.log(Car.model);
+
+
+// if  if-else  else-if
+
+// if(5 > 6)
+// {
+//     console.log("Greater");
+// }
+
+
+// else
+// {
+//     console.log("Smaller");
+// }
+// let a = 10;
+// console.log(a);
+
+
+// let a;
+// console.log(a);
