ac9f2073084a992de73fdc0c64098c3cc2c58010
Jawadbutt861 <jawadbutt861@gmail.com>
2025-07-18 23:21:50 +0500

    updated

---
+++ JS/LOOPS/script.js
@@ -0,0 +1,69 @@
+// console.log("Hello Coders");
+
+// LOOPS
+// For Loop (Commonly used)
+// While Loop
+// Do-While Loop
+
+// For of
+// For in
+// Infinite Loop
+
+// for(//initialization; condition; increment/decrement;)
+// i++ = i + 1
+
+// for(let i = 0; i <= 10; i++)
+// {
+//     console.log( i + " hello world ");
+// }
+
+//WHILE LOOP
+
+
+// while(condition)
+// JS jo h wo line by line
+
+// let i = 11;
+// while(i <= 10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// Do-While
+
+// let i = 11;
+// do{
+//     console.log(i);  
+//     i++; 
+// }
+// while(i <= 10);
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+
+// let num = prompt("Enter Number to print its Table");
+
+// for( let  i = 1; i <= 10; i++)
+// {
+//     console.log(` ${num} * ${i} = ${num * i} `);
+//     // console.log(num + " * " + i + " = " + num*i);
+// }
+
+
+// for(let i = 10; i >= 0; i--)
+// {
+//     console.log( i );
+// }
+
+// 1 to 100 jo even hyn wo print ho k a jyn
+
+for( let  i = 1; i <= 100; i++)
+{
+   if((i %2 === 0))
+   {
+    console.log(i);
+   }
+    
+}
