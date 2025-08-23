648c6c283c591c1b6bb4a202384f0f309ad6bd1e
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-23 23:34:51 +0500

    updated

---
+++ JS/ARRAYS/script.js
@@ -0,0 +1,72 @@
+// Array collection of data , similar data
+// index 0
+// let color1 = "Red";
+// let color2 = "Blue";
+// let arr = [1,2,3,4,5,6,7];
+// let colors = ["Red","Green","Yellow","Grey","Pink","Pink Lips","Cute Laiba"];
+
+// console.log(colors.length);
+// console.log(colors[2]);
+// for(let  i = 0; i < colors.length; i++)
+// {
+//     console.log(colors[i]);  
+//     // console.log(colors[6]);
+// }
+
+//length = 7
+// index = 6
+//color[0] = red
+// i < colors.length
+// i < 7
+
+// For of loop
+// for(let i of colors)
+// {
+//     console.log(i);
+// }
+
+
+// let colors = ["Red","Green","Yellow","Grey","Pink","Pink Lips","Cute Laiba"];
+
+// Array methods
+// push last index k baad add krta h
+// pop remove item from last index
+// shift remove item form first index
+// unshift first index py add krta h
+// splice kisi b jgh pr add or remove krna h to splice ka method use hoga
+
+
+// split
+// slice
+
+// colors.push("Black");
+// console.log(colors);
+
+// colors.pop();
+// console.log(colors);
+
+// colors.shift();
+// console.log(colors);
+
+// colors.unshift("Blue");
+// console.log(colors);
+
+// colors.splice(start index,kitny delete,kia add krna)
+// colors.splice(2,0,"Black","Purple","Sky Blue");
+// console.log(colors);
+
+// let newArray = colors.slice(6)
+// console.log(newArray);
+
+
+// console.log(colors.indexOf("Pink")); agr hoga to index ni to -1
+// console.log(colors.includes("pink")); agr hoga to true ni to false
+
+
+// let cities = ["karachi","quetta","lahore","peshawar","sialkot","rawalpindi"];
+
+// let city = prompt("Enter City");
+// console.log(cities.indexOf(city));
+
+
+
---
+++ JS/REVISION/script.js
@@ -0,0 +1,44 @@
+// let 
+// const
+//  var
+// let  name = "Jawad";
+// name = "Hello";
+// const a = 2;
+// a = 2;
+
+// console.log("Hello Laiba");  used for debugging
+// alert("Hello Jaanuu");
+
+// + - * / % 
+// >= <= > < Comparison
+// += -+ /= *= %= Assignment
+// && || ! Logical
+
+// Conditional Statements 
+// if if-else else-if
+
+
+// Loops
+// for while do-while
+
+// Object 
+// let car = {
+//     name : "Civic",
+//     model : 2008
+// };
+
+// car.company = "Toyota";
+// console.log(car);
+
+// function sum(name){
+//     // console.log(2+2);
+//     return 2+2;
+// }
+
+// sum("Jawad");
+
+
+// let age = prompt("Enter your age");
+// console.log(age);
+
+// confirm("Hello");
