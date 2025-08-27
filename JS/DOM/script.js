9046f5fac27b51058dc08ceabb1d4d554b93632c
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-27 23:26:38 +0500

    updated

--- JS/DOM/script.js
+++ JS/DOM/script.js
@@ -1,14 +1,57 @@
-let head = document.querySelector(".heading");
+// let head = document.querySelector(".heading");
 
-let input = document.querySelector("#input");
+// let input = document.querySelector("#input");
 
-function getvalue()
-{
-    head.innerHTML = `Hello ${input.value}`
-}
+// function getvalue()
+// {
+//     head.innerHTML = `Hello ${input.value}`
+// }
 
 // function change(){
 // head.innerHTML = "Changed using Js";
 // head.style.color = "blue";
 // head.style.fontSize = "60px";
 // }
+
+// let cityInput = document.querySelector("#input")
+// let result = document.querySelector("#result")
+// const cities = [
+//   "karachi",
+//   "lahore",
+//   "islamabad",
+//   "quetta",
+//   "peshawar",
+//   "multan",
+//   "faisalabad",
+//   "rawalpindi",
+//   "hyderabad",
+//   "sialkot",
+//   "gujranwala",
+//   "bahawalpur",
+//   "sukkur",
+//   "mardan",
+//   "abbottabad",
+//   "gwadar",
+//   "murree",
+//   "sargodha",
+//   "okara",
+//   "rahim yar khan"
+// ];
+
+// function chkCity(){
+//     if(cities.includes(cityInput.value.toLowerCase()))
+//     {
+//         // console.log("Included");
+//         result.innerHTML = "Included"
+//         // document.writeln("Included")
+//     }
+//     else{
+//         // console.log("Not Included");   
+//         result.innerHTML = "Not Included"
+//         // document.writeln("Not Included")
+//     }
+    
+// }
+
+// let msg = document.querySelector("#message");
+// msg.innerHTML = "Welcome!";
